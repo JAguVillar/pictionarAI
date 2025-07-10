c7eba0e3faf4aa39c98544174e28defc5c126baf
Agustin Villar <jaguvillar@gmail.com>
2025-07-10 19:07:34 -0300

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -12,33 +12,29 @@ import '@fontsource/dm-sans/600.css'
 import '@fontsource/dm-sans/700.css'
 import '@fontsource/dm-sans/800.css'
 import '@fontsource/dm-sans/900.css'
+
+import '@fontsource/cinzel-decorative/400.css'
+import '@fontsource/cinzel-decorative/700.css'
+import '@fontsource/cinzel-decorative/900.css'
+
+import '@fontsource/cinzel-decorative/400.css'
+import '@fontsource/cinzel-decorative/700.css'
+import '@fontsource/cinzel-decorative/900.css'
 </script>
 
 <template>
-  <header
-    class="h-14 p-4 w-full absolute"
-    :class="
-      $route.meta.absolute
-        ? 'bg-linear-to-b dark:from-black to-transparent bg-transparent'
-        : 'bg-transparent'
-    "
-  >
-    <back-icon
-      v-if="$route.meta.showBack"
-      class="cursor-pointer size-6"
-      @click="$router.back()"
-    ></back-icon>
-    <div v-else>
+  <!-- <header class="h-14 p-4 w-full">
+    <div class="dmsans">
       <h1 class="font-semibold text-xl">
-        Dígalo con Mimic<span
+        Pictionar<span
           class="bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent font-black text-2xl"
           >AI</span
         >
       </h1>
     </div>
-  </header>
+  </header> -->
   <RouterView />
-  <BottomNav />
+  <!-- <BottomNav /> -->
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,142 +1,149 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue'
-import { useInfiniteScroll } from '@vueuse/core'
-import { useRouter, useRoute } from 'vue-router'
-import Item from '@/components/MovieItem.vue'
+import { GoogleGenAI } from '@google/genai'
 
-const router = useRouter()
-const route = useRoute()
-const movies = ref([])
-const page = ref(1)
-const totalPages = ref(Infinity) // se actualizará en la primera llamada
+const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY
+const ai = new GoogleGenAI({
+  apiKey: GEMINI_API_KEY,
+})
 const loading = ref(false)
-const hasMore = ref(true)
-const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY
-const scrollTarget = ref(null) // contenedor observable
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer ' + TMDB_API_KEY,
-  },
+const bgColorClasses = {
+  'yellow-500': 'bg-yellow-500/90',
+  'blue-500': 'bg-blue-500/90',
+  'orange-500': 'bg-orange-500/90',
+  'green-500': 'bg-green-500/90',
+  'red-500': 'bg-red-500/90',
+  'purple-500': 'bg-purple-500/90',
+  'pink-500': 'bg-pink-500/90',
+  'sky-500': 'bg-sky-500/90',
 }
+const defaultWords = [
+  {
+    id: 1,
+    category: 'Persona, animal o lugar',
+    word: 'Taylor Swift',
+    color: 'yellow-500',
+  },
+  {
+    id: 2,
+    category: 'Objeto',
+    word: 'Auriculares inalámbricos',
+    color: 'blue-500',
+  },
+  {
+    id: 3,
+    category: 'Acción',
+    word: 'Maratonear una serie',
+    color: 'orange-500',
+  },
+  {
+    id: 4,
+    category: 'Difícil',
+    word: 'Inteligencia artificial',
+    color: 'green-500',
+  },
+  {
+    id: 5,
+    category: 'Todos Juegan',
+    word: 'Fiesta sorpresa',
+    color: 'red-500',
+  },
+  {
+    id: 6,
+    category: 'Marca o logo',
+    word: 'Spotify',
+    color: 'purple-500',
+  },
+  {
+    id: 7,
+    category: 'Meme o tendencia',
+    word: 'Mujer gritándole a un gato',
+    color: 'pink-500',
+  },
+  {
+    id: 8,
+    category: 'Argenta',
+    word: 'Tomar mate',
+    color: 'sky-500',
+  },
+]
+const words = ref([...defaultWords])
 
-async function loadMore() {
-  if (loading.value || !hasMore.value) return
-
+async function fetchAI() {
   loading.value = true
-  console.log(route.params.listId)
+  try {
+    const response = await ai.models.generateContent({
+      model: 'gemini-2.5-pro',
+      contents: `Quiero que generes una tarjeta de Pictionary compuesta por 8 palabras o frases, cada una perteneciente a las siguientes categorías:
 
-  if (route.params.listId) {
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/list/${route.params.listId}?language=es-MX&page=${page.value}`,
-        options,
-      )
-      const data = await res.json()
-
-      movies.value.push(...data.items)
-
-      // Actualiza paginación
-      totalPages.value = data.total_pages
-      page.value += 1
-      hasMore.value = page.value <= totalPages.value
-    } catch (err) {
-      console.error('Error cargando películas', err)
-      hasMore.value = false
-    } finally {
-      loading.value = false
-    }
-  } else {
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/top_rated?language=es-MX&page=${page.value}`,
-        options,
-      )
-      const data = await res.json()
-
-      movies.value.push(...data.results)
-
-      // Actualiza paginación
-      totalPages.value = data.total_pages
-      page.value += 1
-      hasMore.value = page.value <= totalPages.value
-    } catch (err) {
-      console.error('Error cargando películas', err)
-      hasMore.value = false
-    } finally {
-      loading.value = false
-    }
+🟡 categoria 1, nombre: persona/animal/lugar, color: amarillo, explicacion de la categoria: nombres, persona, animales o lugares.
+categoria 2, nombre: objeto, color: azul, explicacion de la categoria: cosas que se pueden ver o tocar.
+categoria 3, nombre: accion, color: naranja, explicacion de la categoria: cosas que se pueden hacer.
+categoria 4, nombre: dificil, color: verde, explicacion de la categoria: palabras, conceptos o expresiones desafiantes.
+categoria 5, nombre: todos juegan, color: rojo, explicacion de la categoria: puede ser cualquier palabra.
+categoria 6, nombre: Marca o logo, color: purpura, marcas reconocidas o logos visualmente conocidos.
+categoria 7, nombre: Meme o tendencia, color: rosa, explicacion de la categoria: meme popular, tendencia viral o situación reconocible en redes sociales.
+categoria 8, nombre: Argenta, color: cielo, explicacion de la categoria: elemento cultural argentino reconocible, como comidas típicas, modismos, personajes, costumbres o íconos nacionales.
+⚠️ Las palabras deben ser actuales o ampliamente conocidas por el público general. Pueden referirse a cultura pop, tecnología, redes sociales, memes virales o costumbres argentinas populares. Recuerda que no todo tiene que ser de Argentina, pero sí debe ser algo que la mayoría de las personas pueda reconocer o entender.`,
+      config: {
+        responseMimeType: 'application/json',
+        systemInstruction: `
+      Devolvé un JSON, con este formato:
+      [
+      {
+      id: 1,
+      category: "Persona, animal o lugar" o categoria correspondiente,
+      word: "aqui iria la palabra generada de acuerdo a la categoria",
+      color: "yellow-500", // o el color correspondiente a la categoria en formato tailwindcss
+      },
+      ]
+      . Sin explicaciones ni texto adicional. Usá español argentino o neutro. Por ejemplo, decí "celular", no "teléfono móvil".
+      `,
+      },
+    })
+    console.log(response.text)
+    words.value = JSON.parse(response.text)
+  } catch (e) {
+    console.error('Error al generar palabras:', e)
+  } finally {
+    loading.value = false
   }
 }
 
-function goToMovie(id) {
-  router.push({ name: 'movie', params: { movieId: id } })
-}
-
-useInfiniteScroll(scrollTarget, loadMore, { distance: 200 })
-
-onMounted(loadMore)
-
-watch(
-  () => route.params.listId,
-  async () => {
-    // reiniciar el estado cuando cambia el parámetro
-    movies.value = []
-    page.value = 1
-    totalPages.value = Infinity
-    hasMore.value = true
-    await loadMore()
-  },
-)
+onMounted(() => {})
 </script>
-
 <template>
-  <main class="main">
-    <div class="mb-6">
-      <h1 class="text-3xl font-bold">
-        {{ route.meta.title }}
+  <main class="min-h-screen flex flex-col justify-center items-center p-4 main">
+    <div class="flex flex-col flex-1 w-full gap-y-2">
+      <h1
+        class="font-black text-3xl text-center flex-1 flex justify-center items-center"
+        @click="fetchAI"
+      >
+        <span class="cinzel"> pictionar </span>
+        <span
+          class="bg-linear-to-bl from-violet-500 to-fuchsia-500 bg-clip-text text-transparent font-black cinzel"
+          >AI</span
+        >
       </h1>
-      <h2 class="text-xl font-semibold text-gray-600">
-        {{ route.meta.subTitle }}
-      </h2>
-    </div>
-    <div ref="scrollTarget" class="h-[90vh] overflow-auto">
-      <div class="grid grid-cols-3 sm:grid-cols-3 gap-x-4 gap-y-6">
-        <div
-          v-for="movie in movies"
-          :key="movie.id"
-          class="transition duration-200 hover:scale-[1.02] hover:shadow-xl"
-        >
-          <Item
-            @click="goToMovie(movie.id)"
-            :title="movie.title"
-            :poster="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-          />
+      <div
+        v-for="word in words"
+        :key="word.id"
+        class="rounded-lg text-black flex-1 grid grid-rows-[auto_1fr] text-center"
+        :class="[bgColorClasses[word.color]]"
+      >
+        <div class="bg-black/35">
+          <h1 class="text-lg font-bold dmsans">
+            {{ word.category }}
+          </h1>
         </div>
-      </div>
-
-      <div v-if="loading" class="flex justify-center py-6">
-        <svg
-          class="animate-spin h-6 w-6 text-indigo-500"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            class="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            stroke-width="4"
-          />
-          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z" />
-        </svg>
-      </div>
-
-      <div v-if="!hasMore && !loading" class="py-6 text-center text-gray-500">
-        Nada más para mostrar
+        <div class="flex justify-center items-center">
+          <h2
+            class="text-2xl font-black dmsans transition-opacity duration-300"
+            :class="loading ? 'opacity-30' : 'opacity-100'"
+          >
+            {{ loading ? '✨Generando palabras...✨' : word.word }}
+          </h2>
+        </div>
       </div>
     </div>
   </main>
